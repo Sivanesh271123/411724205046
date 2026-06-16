4ff7f9d873c793d8d9283d8e227560c476aae14a
sivanesh39289-byte <sivanesh.e.m@gmail.com>
2026-06-16 11:19:26 +0530

    js

---
+++ june13.js
@@ -0,0 +1,6 @@
+function add(){let a=Number(document.getElementById("num1").value);
+let b=Number(document.getElementById("num2").value);
+let sum=a+b;
+console.log(sum);
+document.getElementById("Result").textContent "RESULT: "+sum;
+}
